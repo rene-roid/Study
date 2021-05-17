9e9868141a26ac9d1503a000d8d663d846080811
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:58:28 +0200

    Create 8-ListsFunctions.py

---
+++ PythonCourse/8-ListsFunctions.py
@@ -0,0 +1,7 @@
+lucky = [0, 2]
+friends = ['noneone', 'nadie', 'jaja noob']
+friends.extend(lucky)
+friends.append('NNASDJDNAS')
+friends.insert(1, 'pepe')
+friends.remove('NNASDJDNAS')
+print(friends)
