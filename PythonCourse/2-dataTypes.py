71cbcea4cc06e48b3375cfbdc4d60dc044c505f0
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:58:13 +0200

    Create 2-dataTypes.py

---
+++ PythonCourse/2-dataTypes.py
@@ -0,0 +1,11 @@
+character_name = "Jhon" #string
+character_age = "27"
+isMale = True
+
+print('There was a guy called ' + character_name + ", ")
+print('He is ' + character_age + ' years old')
+
+character_name = "Pepe"
+character_age = 30
+print("He didn't like his name so he changed it to " + character_name)
+print('But ' + character_name + 'changed his name when he was ')
