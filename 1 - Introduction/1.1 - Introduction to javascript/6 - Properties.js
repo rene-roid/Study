fd79f8335f3ba57dd8624a3aa6d4e8b9a6003a8c
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:45:57 +0200

    Delete 6 - Properties.js

--- 1 - Introduction/1.1 - Introduction to javascript/6 - Properties.js
+++
@@ -1,5 +0,0 @@
-// When you add a new piece of data into a JavaScript program, the browser saves it as a data type.
-// Every string instance has a property called length that stores the number of characters that that string has
-console.log('WHAT!?'.length)
-
-// The . is another operator, it is called the dot operator
