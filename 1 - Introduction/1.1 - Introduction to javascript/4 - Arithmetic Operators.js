176f929ba409d0058544aabe09100ea9c374b111
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:41:15 +0200

    Update 4 - Arithmetic Operators.js

---
+++ 1 - Introduction/1.1 - Introduction to javascript/4 - Arithmetic Operators.js
@@ -0,0 +1,17 @@
+// Basic arithmetic often comes in handy when programming.
+
+// There are 5 basic arithmetic operators:
+// 1 - Add: +
+console.log(1 + 1);
+
+// 2 - Substract: -
+console.log(10 - 5);
+
+// 3 - Multiply: *
+console.log(6 * 6);
+
+// 4 - Divide: -
+console.log(9 / 3);
+
+// 5 - Remainder: %
+console.log(11 % 3);
