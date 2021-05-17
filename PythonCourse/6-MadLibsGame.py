3d75e78151c37472f8d44ae8c0ca4b770f5a9faa
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:58:24 +0200

    Create 6-MadLibsGame.py

---
+++ PythonCourse/6-MadLibsGame.py
@@ -0,0 +1,8 @@
+color = input('Give me a color: ')
+plural_noun = input('Gibe me a plural: ')
+celebrity = input('enter a celebrity: ')
+
+print('Roses are ' + color)
+print(plural_noun + ' are blue')
+print('That is why, i ' + celebrity + ' you')
+
