57d492343b4c3d71e7e9f27ff7a6b9873ea24ecc
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-05-17 22:45:45 +0200

    Delete 1 - Console.js

--- 1 - Introduction/1.1 - Introduction to javascript/1 - Console.js
+++
@@ -1,7 +0,0 @@
-// The console is a panel that displays important messages, like errors.
-// Most of the code in JS is invisible by default, but we can make it appear on the console printing it.
-// In JavaScript to print anything, we use console.log()
-
-console.log(18);
-
-// In this example the console logs the number 18
