42a2df7740f5f6d6e49644cce056460e5d88f11b
Yuuki <52652158+rene-roid@users.noreply.github.com>
2021-09-22 22:29:57 +0200

    New path

---
+++ opps (outdated)/1 - Introduction/1.1 - Introduction to javascript/2 - Comments.js
@@ -0,0 +1,20 @@
+// In programming your code may be difficult to understand. 
+// As we write code, we can add comments, that is code that the computer will ignore when you run your code.
+// This is very usefull to explain the things that happen on your code.
+
+// A single line comment is denoted with two foward slashes //
+
+// Printing 69 to the console
+console.log(69); // You can also use single line comments after a line of code
+
+// A multi line comment is denotes with a /* to beguin the comment and to edit with a */
+
+/*
+This is a multi line comment
+console.log('Hello World!')
+
+This code will be ignored!
+*/
+
+// You can also add commenst in the middle of the code
+console.log(/*WOW this is an eepic comment*/ 69);
